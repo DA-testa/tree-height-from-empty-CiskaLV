b90125eaa4fec33fcb4003c4d0f00703940421a7
Ciska <44933905+CiskaLV@users.noreply.github.com>
2023-03-06 00:04:52 +0200

    Saving hights of previusly calculated nodes and swithc to numpy for faster lookups

--- tree_height.py
+++ tree_height.py
@@ -1,32 +1,38 @@
 # python3
 
 import sys
-import threading
+# import threading
 import numpy
-
 
 def compute_height(n, parents):
     max_height = 0
+    heights = numpy.full((n,), -1)
 
     for i in range(n):
-        height = 0
-        current = i
-        while current != -1:
-            current = parents[current]
-            height += 1
-        max_height = max(max_height, height)
-        
-    print(max_height)
+        if  heights[i] != -1:
+            height = heights[i]
+        else:
+            height = 0
+            current = i
+            while current != -1:
+                if heights[current] != -1:
+                    height += heights[current]
+                    break
+                current = parents[current]
+                height += 1
+            heights[i] = height
+        max_height = numpy.max([max_height, height])
+
+    return max_height
 
 def main():
     text = input()
-
+    number = int
+    parents = numpy.array
     if text[0] == "I":
         print("Enter number of nodes: ")
         number = int(input())
-        parents = list(map(int, input().split()))
-        print(parents)
-        compute_height(number, parents)
+        parents = numpy.array(map(int, input().split()))
 
     elif text[0] == "F":
         fileName = input()
@@ -38,27 +44,17 @@ def main():
                 fileName = "test/" + fileName
             file = open(fileName, "r")
             number = int(file.readline())
-            parents = list(map(int, file.readline().split()))
-            compute_height(number, parents)
+            parents = numpy.array(list(map(int, file.readline().split())))
 
     else:
         print("Invalid input")
         return
-    # implement input form keyboard and from files
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    print(compute_height(number, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-# threading.Thread(target=main).start()
+# threading.stack_size(2**27)   # new thread will get stack of such size
 main()
-# print(numpy.array([1,2,3]))
