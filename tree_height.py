dc0fbd66355a17cc67bc887273e2096dd4e263ba
Ciska <44933905+CiskaLV@users.noreply.github.com>
2023-03-05 22:22:04 +0200

    Quick test

--- tree_height.py
+++ tree_height.py
@@ -6,13 +6,42 @@ import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
     max_height = 0
-    # Your code here
-    return max_height
 
+    for i in range(n):
+        height = 0
+        current = i
+        while current != -1:
+            current = parents[current]
+            height += 1
+        max_height = max(max_height, height)
+        
+    print(max_height)
 
 def main():
+    text = input()
+
+    if text[0] == "I":
+        print("Enter number of nodes: ")
+        number = int(input())
+        parents = list(map(int, input().split()))
+        print(parents)
+        compute_height(number, parents)
+
+    elif text[0] == "F":
+        fileName = input()
+        if "a" in fileName:
+            print("Invalid file name")
+            return
+        else:
+            file = open(fileName, "r")
+            number = int(file.readline())
+            parents = list(map(int, file.readline().split()))
+            compute_height(number, parents)
+
+    else:
+        print("Invalid input")
+        return
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -28,6 +57,6 @@ def main():
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+# threading.Thread(target=main).start()
 main()
 # print(numpy.array([1,2,3]))
